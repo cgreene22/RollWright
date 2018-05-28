eb16ee25d9613fc50c686238bde39dcf2b7982ab
Christopher Greene <c.greene1@gmail.com>
2018-05-27 23:59:19 -0230

    almost finished now

--- rolls.js
+++ rolls.js
@@ -17,40 +17,77 @@ function flip() {
     print("Coin Tossed: " + toss);
 }
 
+function findMath(request) {
+    var test = false;
+    // It took a frustrating amount of time to discover this
+    // particular idiosyncracy of JS, namely that special Regex
+    // characters need to be double escaped to find matches.
+    // Fun. So much fun.
+    var checkPlus = new RegExp('\\++');
+    if (checkPlus.test(request)) {
+        test = true;
+    }
+    var checkMinus = new RegExp('\\-+');
+    if (checkMinus.test(request)) {
+        test = true;
+    }
+    var checkMulti = new RegExp('\\*+');
+    if (checkMulti.test(request)) {
+        test = true;
+    }
+    var checkDivi = new RegExp('\\/+');
+    if (checkDivi.test(request)) {
+        test = true;
+    }
+    return test;
+}
+
 function rollCustom() {
     query = CUSTOM.value;
     // Separate the modifiers
     var modFlag = false;
-    var terms = [];
-    while (!modFlag) {
-        /*
-        var i = 0;
-        var check = /[0-9]?d?/i;
-        if (check.test(query.charAt(i))) {
-            i++;
+    var rolls = "";
+    var modifiers = "";
+    var i = 0;
+    try {
+        while (!modFlag) {
+            if (!findMath(query)) {
+                rolls = query;
+                modFlag = true;
+                console.log("Rolls: " + rolls);
+            }
+            else if (findMath(query[i])) {
+                rolls = query.substring(0, i);
+                modifiers = query.substring(i, query.length);
+                console.log("Rolls: " + rolls);
+                console.log("Mods: " + modifiers);
+                modFlag = true;
+            }
+            else {
+                i++;
+            }
         }
-        else {
-            request = query.slice(0, i-1);
-            console.log(request);
-            terms[1] = query.substring(i, query.length);
-            console.log(terms[1]);
-            modFlag = true;
-        }
-        */
+    }
+    catch (error) {
+        console.log(error);
+    }
+    var check2 = new RegExp('^[0-9]+d[0-9]+$', 'i');
+    if (!check2.test(rolls)) {
+        print("Proper formatting is [number] d [number], like 3d6, with any math after (like 3d6+3).");
     }
     // Get the number and type of dice
-    var dice = query.split("d");
-    var rolls = dice[0];
+    /*var dice = rolls.split("d");
+    var toRoll = dice[0];
     var die = dice[1];
-    var count= [];
+    var count = [];
     var total = 0;
-    for (var c = 0; c < rolls; c++) {
+    for (var c = 0; c < toRoll; c++) {
         var rolled = roll(die);
         count[c] = " " + rolled;
         total += rolled;
     }
     var printRolls = count.toString();
-    print("Rolled " + rolls + "d" + die + ":" + printRolls + ". Total: " + total);
+    print("Rolled " + rolls + "d" + die + ":" + printRolls + ". Total: " + total); */
 }
 
 function print(s, i) {
